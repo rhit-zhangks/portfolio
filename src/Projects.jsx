c2aa9f158621c4eae37e9bba28ce4c0e504a53b2
kallista <148663001+rhit-zhangks@users.noreply.github.com>
2025-07-24 14:27:54 -0400

    minor tweaks + fix links

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -41,7 +41,7 @@ const Projects = () => {
         <Col span={6}>
           <ProjectCard
             image="/inventoryManager.png"
-            imageDesc="The homepage for the cafe inventory manager that lists ingredient cards."
+            imageDesc="The detail page for whole milk showing edit history."
             title={
               <h1
                 style={{
@@ -53,11 +53,11 @@ const Projects = () => {
             }
             description={
               <p>
-                An online application for tracking ingredient counts with edit
+                An online application for tracking inventory counts with edit
                 history and account verification. Uses Firebase for user
                 authentification and storage. Records a history of changes as
                 well as who changes were made by, allowing users to input the
-                price they bought ingredients at. View the website at{" "}
+                price they bought ingredients at. Try it out yourself at{" "}
                 <a
                   target="_blank"
                   href="https://cafe-inventory-manager.web.app/"
@@ -72,7 +72,7 @@ const Projects = () => {
         <Col span={6}>
           <ProjectCard
             image="/sunsetDetector.png"
-            imageDesc="The homepage for the cafe inventory manager that lists ingredient cards."
+            imageDesc="The front page of my Sunset Detector IEEE paper."
             title={
               <h1
                 style={{
@@ -84,16 +84,16 @@ const Projects = () => {
             }
             description={
               <p>
-                Trained three models to classify sunsets. First using a more
+                A machine learning project where I trained three models to classify sunsets. The first used a more
                 traditional image recognition style of extracting features and
-                optimizing hyperparameters for an SVM. Features were extracted
-                through LST conversion & standardization. Secondly, we utilize
+                optimizing hyperparameters for a support vector machine (SVM). Features were extracted
+                through LST conversion & standardization. The second utilized
                 AlexNet, experimenting with both feature extraction from one of
                 the final layers of AlexNet and transfer learning of its final
                 layers.{" "}
                 <a
                   target="_blank"
-                  href="https://cahttps://docs.google.com/document/d/1_SSlWuaqTrZvJRTHeFDanx7JXKpK23D_p7NHPuvS_f4/edit?usp=sharingfe-inventory-manager.web.app/"
+                  href="https://docs.google.com/document/d/1NHrTAmSWwWwIIfITVJ68XA2OJMFqOv9NImNsgdZmSFg/edit?usp=sharing"
                 >
                   Read the paper here!
                 </a>
@@ -105,7 +105,7 @@ const Projects = () => {
         <Col span={6}>
           <ProjectCard
             image="/inventoryManager.png"
-            imageDesc="The homepage for the cafe inventory manager that lists ingredient cards."
+            imageDesc="The cat search page for CatFinder, a website that allows users to see nearby shelters and cats."
             title={
               <h1
                 style={{
